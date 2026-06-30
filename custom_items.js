6064d05c6450276434ad4f730a62ac468bf8ee40
tahaoplokmno-cell <tahaoplokmno@gmail.com>
2026-07-01 02:00:24 +0300

    Create custom_items.js

---
+++ custom_items.js
@@ -0,0 +1,50 @@
+const MY_CUSTOM_GAMES = {
+    "PUBG Mobile": [
+        "كود 60 شده - 1.00$", 
+        "كود 325 شده - 4.90$", 
+        "كود 660 شده - 9.80$", 
+        "كود 1800 شده - 24.50$"
+    ],
+    "Free Fire": [
+        "100+10 جوهرة - 1.10$", 
+        "200+21 جوهرة - 2.00$", 
+        "530+53 جوهرة - 5.22$", 
+        "1080+108 جوهرة - 10.16$"
+    ],
+    "Call of Duty": [
+        "320 CP - 5.00$", 
+        "480 CP - 7.30$", 
+        "560 CP - 8.50$"
+    ],
+    "Clash of Clans": [
+        "88 Gems - 1.50$", 
+        "500 Gems - 7.00$"
+    ],
+    "Minecraft": [
+        "1720 Coins - 10.00$", 
+        "3500 Coins - 20.00$"
+    ],
+    "Roblox": [
+        "Roblox 11$ - 11.00$", 
+        "Robux 16$ - 16.00$"
+    ],
+    "Stumble Guys": [
+        "250 Gems - 1.20$", 
+        "800 Gems - 3.00$", 
+        "1600 Gems - 5.00$"
+    ]
+};
+
+const MY_CUSTOM_CARDS = {
+    "Steam USA": [
+        "Steam 5.3$ - 5.30$", 
+        "Steam 10.3$ - 10.30$"
+    ],
+    "XBOX USA": [
+        "XBOX 10.5$ - 10.50$", 
+        "XBOX 15.5$ - 15.50$"
+    ]
+};
+
+module.exports = { MY_CUSTOM_GAMES, MY_CUSTOM_CARDS };
+
