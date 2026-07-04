05f62ae1aea2b8a35b908febf7f4d2e2b3fd7b94
tahaoplokmno-cell <tahaoplokmno@gmail.com>
2026-07-04 03:55:15 +0300

    Update menus.js

--- menus.js
+++ menus.js
@@ -1,17 +1,21 @@
 const { Markup } = require('telegraf');
 
 const mainMenu = Markup.keyboard([
-    ['🏪 المتجر'], ['💳 المحفظة', '💰 استرجاع الأموال'], ['⚙️ الإعدادات', '📞 الدعم الفني']
+    ['🏪 المتجر'], 
+    ['💳 المحفظة', '💰 استرجاع الأموال'], 
+    ['⚙️ الإعدادات', '📞 الدعم الفني']
 ]).resize();
 
 const storeMenu = Markup.inlineKeyboard([
-    [Markup.button.callback("🎮 قسم الألعاب والستور", "m#games")], 
-    [Markup.button.callback("🎟️ قسم البطاقات الرقمية", "m#cards")], 
-    [Markup.button.callback("🤖 إنشاء وتصميم بوت خاص", "bot_order#start")]
+    [Markup.button.callback("🎮 قسم الألعاب", "m#games")],
+    [Markup.button.callback("🎟️ بطاقات ستيم وإكس بوكس", "m#cards")],
+    [Markup.button.callback("📱 شحن رصيد هاتف", "m#phone")],
+    [Markup.button.callback("🤖 إنشاء بوت", "bot_order#start")]
 ]);
 
 const walletMenu = Markup.inlineKeyboard([
-    [Markup.button.callback("💵 شحن بالدولار", "ch#usd"), Markup.button.callback("🇸🇾 شحن بالليرة السورية", "ch#syr")]
+    [Markup.button.callback("💵 شحن بالدولار", "ch#usd"), 
+     Markup.button.callback("🇸🇾 شحن بالليرة", "ch#syr")]
 ]);
 
 const chargeValuesMenu = Markup.inlineKeyboard([
